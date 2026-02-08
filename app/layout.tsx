78b588e822132eb02cee4363e2cb654899223ea5
Andre Ribeiro <adaguiasdagracadir@gmail.com>
2026-02-08 17:14:33 +0000

    fix layout root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import "./globals.css";
-import Navbar from "./components/Navbar";
 import type { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Área Reservada",
+  description: "Sistema interno de gestão de sócios",
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
-return (
-<html lang="pt">
-  </body>
-  <Navbar />
-  {children}
-  </body>
-  </html>
-);
+  return (
+    <html lang="pt">
+      <body>{children}</body>
+    </html>
+  );
 }
