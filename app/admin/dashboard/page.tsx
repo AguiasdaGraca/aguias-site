8634913ba4e4baa0430b36fa71839c93e15d1f34
Andre Ribeiro <adaguiasdagracadir@gmail.com>
2026-02-09 20:45:06 +0000

    fix final import path

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,14 +1,12 @@
-import { createClient } from "../..//lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-  const supabase = createClient();
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  // 🔒 Se não existir sessão → volta ao login
   if (!session) {
     redirect("/admin");
   }
@@ -16,7 +14,7 @@ export default async function Dashboard() {
   return (
     <main style={{ padding: "40px" }}>
       <h1>Dashboard</h1>
-      <p>Bem-vinda à área interna 😎</p>
+      <p>Bem-vinda à área interna 😇</p>
     </main>
   );
 }
